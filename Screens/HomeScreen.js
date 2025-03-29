e2828314003747f56bb0248a2ccb17d683f2f105
neccajoy11 <neccadayucos@gmail.com>
2025-03-29 13:51:37 +0800

    first commit

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+
+  const [count, setCount] = useState (0);
+  
+  return (
+    <View style={styles.container}>
+      <Text style= {styles.text}>Necca Joy</Text>
+      <Text style= {styles.text}>You Click {count} times</Text>
+      <Button title="Click Me" onPress={() => setCount(count + 1)}/>
+      <Button title="Go Details" onPress={() => navigation.navigate('Details')}></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+});
+
+export default HomeScreen;
